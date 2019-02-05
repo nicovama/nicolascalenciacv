881dded672a7974f5b003ddccd2a8a97fea00370
Nicolas Valencia <nicolas.valencia@sony.com>
2019-02-05 16:07:22 +0900

    update files

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -33,8 +33,8 @@ class Header extends Component {
 
       <div className="row banner">
          <div className="banner-text">
-            <h1 className="responsive-headline">I'm {name}.</h1>
-            <h3>I'm a {state} based <span>{occupation}</span>. {description}.</h3>
+            <h1 className="responsive-headline">I am {name}</h1>
+            <h3>I am {state} based <span>{occupation}</span>. {description}.</h3>
             <hr />
             <ul className="social">
                {networks}
